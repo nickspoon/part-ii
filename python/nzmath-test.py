d33a5f3488cce71859c9f3fa7b34dee88bc55de6
Nick Spooner <nickspoon0@gmail.com>
2013-03-05 01:14:12 +0000

    Graph examples and stable colouring programs

--- python/nzmath-test.py
+++ python/nzmath-test.py
@@ -1,6 +1,7 @@
 from nzmath import finitefield, matrix, vector
 from nzmath.poly import ring as polyring
 from nzmath.poly.uniutil import polynomial
+import numpy as np
 
 f2 = finitefield.FinitePrimeField(2)
 x = f2.createElement(0)
@@ -38,3 +39,8 @@ def T(A, j):
     
 A = matrix.Matrix(2, 2, [1, 0, 1, 0], f2)
 print T(A, 1)
+
+M = np.array([ f2.createElement(x) for x in [ 1, 0, 1, 0 ] ], dtype=object)
+M = M.reshape(2,2)
+print
+print M + f2.one
